f0198ea8e2d424231a5ce289ba8004e94a6ced7c
Sun Geer <1013296326@qq.com>
2024-02-01 22:43:25 +0800

    Update basedb.py

--- basedb.py
+++ basedb.py
@@ -1,98 +1,145 @@
 import math
-from functools import lru_cache
+import re
 
-import aiomysql
+import MySQLdb
+from MySQLdb.cursors import DictCursor  # python -m pip install mysqlclient
+from dbutils.pooled_db import PooledDB  # python -m pip install DBUtils
+
+from fairy.config import db_settings
+
+dbpool = PooledDB(
+    creator=MySQLdb,
+    maxcached=5,
+    host=db_settings.DB_HOST,
+    port=db_settings.DB_PROT,
+    db=db_settings.DB_NAME,
+    user=db_settings.DB_USER,
+    passwd=db_settings.DB_PASS,
+    charset='utf8mb4',
+    cursorclass=DictCursor
+)
 
 
-async def _create_dbpool():
-    dbpool = await aiomysql.create_pool(
-        host=CONF.get_conf('DATABASE', 'host'),
-        port=int(CONF.get_conf('DATABASE', 'port')),
-        db=CONF.get_conf('DATABASE', 'name'),
-        user=CONF.get_conf('DATABASE', 'user'),
-        password=CONF.get_secconf('DATABASE', 'passwd')
+def create_dbconn_mysql():
+    conn = dbpool.connection()
+    return conn
+
+
+def cur():
+    conn = MySQLdb.connect(
+        host=db_settings.DB_HOST,
+        port=db_settings.DB_PROT,
+        db=db_settings.DB_NAME,
+        user=db_settings.DB_USER,
+        passwd=db_settings.DB_PASS,
+        charset='utf8mb4',
+        cursorclass=DictCursor
     )
-    return dbpool
-
-
-@lru_cache
-async def get_dbpool():
-    return await _create_dbpool()
+    conn.text_factory = str
+    return conn
 
 
 class BaseModel:
-
     def __init__(self):
-        self.__conn = None
-        self.cur = None
+        self._conn = None
+        self.cursor = None
 
-    @classmethod
-    async def _initialize_pool(cls):
-        if not hasattr(cls, '__pool'):
-            cls.__pool = await get_dbpool()
+    def conn(self):
+        if not self.cursor:
+            if not self._conn:
+                self._conn = create_dbconn_mysql()
+            self.cursor = self._conn.cursor()
 
-    async def conn(self):
-        await self._initialize_pool()
-        if not self.cur:
-            if not self.__conn:
-                self.__conn = await self.__pool.acquire()
-            self.cur = await self.__conn.cursor()
+    def rollback(self):
+        if self._conn:
+            self._conn.rollback()
 
-    async def begin(self):
-        await self.__conn.begin()
-
-    async def rollback(self):
-        await self.__conn.rollback()
-
-    async def execute(self, sql_str, values=None):
+    def commit(self):
         try:
-            await self.cur.execute(sql_str, values)
+            self._conn.commit()
         except Exception as e:
-            await self.rollback()
-            await self.close()
-            raise ConnectionAbortedError(str(e))
+            self.rollback()
+            raise Exception('db commit failed:\n{}'.formate(e))
 
-    async def executemany(self, sql_str, values=None):
-        try:
-            await self.cur.executemany(sql_str, values)
-        except Exception as e:
-            await self.rollback()
-            await self.close()
-            raise ConnectionAbortedError(str(e))
+    def begin(self):
+        if self._conn:
+            self._conn.begin()
 
-    async def commit(self):
+    def close(self):
         try:
-            await self.__conn.commit()
+            if self._conn:
+                if self.cursor:
+                    self.cursor.execute('UNLOCK TABLES;')
+                    self.cursor.close()
+                self._conn.close()
         except Exception as e:
-            await self.rollback()
-            raise ConnectionAbortedError(str(e))
-
-    async def close(self):
-        try:
-            if self.cur:
-                await self.cur.execute('UNLOCK TABLES;')
-                await self.cur.close()
-        except Exception as e:
-            raise ConnectionAbortedError(str(e))
+            raise Exception('db close failed:\n{}'.formate(e))
         finally:
-            self.__pool.release(self.__conn)
-            self.cur = None
-            self.__conn = None
+            self.cursor = None
+            self._conn = None
+
+    def execute(self, sql_str, values=None):
+        try:
+            self.cursor.execute(sql_str, values)
+        except Exception as e:
+            self.rollback()
+            self.close()
+            raise Exception('db execute failed:\n{}'.formate(e))
+
+    def executemany(self, sql_str, values=None):
+        try:
+            self.cursor.executemany(sql_str, values)
+        except Exception as e:
+            self.rollback()
+            self.close()
+            raise Exception('db executemany failed:\n{}'.formate(e))
+
+
+class DBConnection:
+    def __init__(self):
+        self._conn = None
+        self.cursor = None
+
+    def commit(self):
+        self._conn.commit()
+
+    def __enter__(self):
+        try:
+            if not self.cursor:
+                if not self._conn:
+                    self._conn = create_dbconn_mysql()
+                self.cursor = self._conn.cursor()
+        except:
+            raise Exception('db connection failed')
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        try:
+            if self._conn:
+                if self.cursor:
+                    self.cursor.execute('UNLOCK TABLES;')
+                    self.cursor.close()
+                self._conn.close()
+        except:
+            raise Exception('db close failed')
+        finally:
+            self._conn = None
+            self.cursor = None
 
 
 class Common:
     @staticmethod
-    async def parse_limit_str(page_info=None):
+    def parse_limit_str(page_info=None):
         if page_info is None:
             page_info = {}
         page = int(page_info.get('page', 1))
         page_size = int(page_info.get('rows', 20))
-        limit_str = f' LIMIT {(page - 1) * page_size}, {page_size} '
+        limit_str = ' LIMIT %s, %s ' % ((page - 1) * page_size, page_size)
         return limit_str
 
     @staticmethod
-    async def parse_update_str(table, p_key, p_id, update_dict):
-        sql_str = f' UPDATE {table} SET '
+    def parse_update_str(table, p_key, p_id, update_dict):
+        sql_str = ' UPDATE %s SET ' % (table,)
         temp_str = []
         sql_values = []
         for key, value in update_dict.items():
@@ -103,8 +150,8 @@ class Common:
         return sql_str, sql_values
 
     @staticmethod
-    async def parse_where_str(filter_fields, request_data):
-        if not isinstance(filter_fields, (tuple, list)):
+    def parse_where_str(filter_fields, request_data):
+        if not isinstance(filter_fields, tuple) and not isinstance(filter_fields, list):
             filter_fields = (filter_fields,)
         where_str = ' WHERE 1 = %s '
         where_values = [1]
@@ -118,8 +165,8 @@ class Common:
         return where_str, where_values
 
     @staticmethod
-    async def parse_where_like_str(filter_fields, request_data):
-        if not isinstance(filter_fields, (tuple, list)):
+    def parse_where_like_str(filter_fields, request_data):
+        if not isinstance(filter_fields, tuple) and not isinstance(filter_fields, list):
             filter_fields = (filter_fields,)
         where_str = ' WHERE 1 = %s '
         where_values = [1]
@@ -127,19 +174,19 @@ class Common:
             value = request_data.get(key)
             if value:
                 where_str += ' AND ' + key + ' LIKE %s '
-                where_values.append(f'%%%%{value}%%%%')
+                where_values.append('%%%%%s%%%%' % value)
         if not where_values:
             where_values = None
         return where_str, where_values
 
     @staticmethod
-    async def get_page_info(cursor, sql_str, where_values=None, truncate=False, page=1, per_page=20):
+    def get_page_info(cursor, sql_str, where_values=None, truncate=False, page=1, per_page=20):
         page = int(page)
         per_page = int(per_page)
 
         if truncate:
             if 'GROUP BY' in sql_str:
-                sql_str = f'SELECT COUNT(*) total FROM ({sql_str}) AS TEMP'
+                sql_str = 'SELECT COUNT(*) total FROM (%s) AS TEMP' % sql_str
             else:
                 sql_str = re.sub(r'SELECT[\s\S]*?FROM', 'SELECT COUNT(*) total FROM', sql_str, count=1)
 
@@ -151,10 +198,10 @@ class Common:
 
         # 执行查询以获得总记录数
         if where_values:
-            await cursor.execute(sql_str, where_values)
+            cursor.execute(sql_str, where_values)
         else:
-            await cursor.execute(sql_str)
-        total = await cursor.fetchone()['total']
+            cursor.execute(sql_str)
+        total = cursor.fetchone()['total']
 
         # 计算分页信息
         pages = math.ceil(total / per_page)
